272320cdef4707445358e5829cfae6e742d6188d
Jose Enrique <josehn.1466@gmail.com>
2020-12-17 04:48:56 -0500

    El verdadero holis

---
+++ app/src/main/java/com/dam/practicando001/MainActivity.java
@@ -0,0 +1,26 @@
+package com.dam.practicando001;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnLogin = findViewById(R.id.buttonLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(i);
+            }
+        });
+    }
+}
